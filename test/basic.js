008c16ac6aa97f1d824a60567918f4a60060efcf
Matt Bornski <matt@bornski.com>
2012-05-09 19:24:05 -0700

    First draft node module which uses Hyper Log Log to estimate set cardinality

--- test/basic.js
+++ test/basic.js
@@ -10,32 +10,52 @@ var assert = require('assert');
 var harness = require('./harness');
 var cardinality = require('../lib/index');
 
-describe('Log Log algorithm', function () {
-  it('should estimate the cardinality of variously sized sets within 2% accuracy, faster than naive counting', function (done) {
+describe('Hyper Log Log algorithm', function () {
+  it('with short words', function (done) {
     var sizes = [
-      100,
-      10000,
       1000000,
-      10000000,
     ];
-    var results = harness.compare(harness.naiveCardinality, cardinality.LogLog, sizes);
-    console.log('LogLog');
-    console.log(results);
-    return done();
+    var results = harness.compare(harness.heavilyOverlappingShortWords, harness.naiveCardinality, cardinality.set, sizes);
+    var acceptable = true;
+    for (var index in sizes) {
+      console.log('for set of size ~' + sizes[index] + '...');
+      console.log('  HyperLogLog estimated ' + results[index]['counts'][1] + ' items in ' + results[index]['times'][1] + ' ms');
+      console.log('  Naive counting netted ' + results[index]['counts'][0] + ' items in ' + results[index]['times'][0] + ' ms');
+      var deviation = (Math.abs(results[index]['counts'][0] - results[index]['counts'][1]) / results[index]['counts'][0]);
+      console.log(deviation);
+      if (deviation > 0.02) {
+        acceptable = false
+      }
+    }
+    if (!acceptable) {
+      return done(new Error('Results were unacceptably off of real values'));
+    } else {
+      return done();
+    }
   });
 });
 
 describe('Hyper Log Log algorithm', function () {
-  it('should estimate the cardinality of variously sized sets within 2% accuracy, faster than naive counting', function (done) {
+  it('with BSON object id strings', function (done) {
     var sizes = [
-      100,
-      10000,
       1000000,
-      10000000,
     ];
-    var results = harness.compare(harness.naiveCardinality, cardinality.HyperLogLog, sizes);
-    console.log('HyperLogLog');
-    console.log(results);
-    return done();
+    var results = harness.compare(harness.lightlyOverlappingObjectIds, harness.naiveCardinality, cardinality.set, sizes);
+    var acceptable = true;
+    for (var index in sizes) {
+      console.log('for set of size ~' + sizes[index] + '...');
+      console.log('  HyperLogLog estimated ' + results[index]['counts'][1] + ' items in ' + results[index]['times'][1] + ' ms');
+      console.log('  Naive counting netted ' + results[index]['counts'][0] + ' items in ' + results[index]['times'][0] + ' ms');
+      var deviation = (Math.abs(results[index]['counts'][0] - results[index]['counts'][1]) / results[index]['counts'][0]);
+      console.log(deviation);
+      if (deviation > 0.02) {
+        acceptable = false
+      }
+    }
+    if (!acceptable) {
+      return done(new Error('Results were unacceptably off of real values'));
+    } else {
+      return done();
+    }
   });
 });
--- test/harness.js
+++ test/harness.js
@@ -1,21 +1,37 @@
-var generateWords = function (count) {
-  var result = [];
-
-  while (count > 0) {
-    var word = '';
-    for (var j = 0; j < (parseInt(Math.random() * (8 - 1)) + 1); j++) { // from 1char to 8chars
-      word += String.fromCharCode(parseInt(Math.random() * (122 - 97)) + 97); // a-z
-    }
-
-    for (var i = 0; i < Math.random() * 100; i++) {
-      result.push(word);
-      count--;
-    }
-  }
-  return result;
-};
-
 module.exports = {
+  'heavilyOverlappingShortWords': function (count) {
+    var result = [];
+
+    while (count > 0) {
+      var word = '';
+      for (var j = 0; j < (parseInt(Math.random() * (8 - 1)) + 1); j++) { // from 1char to 8chars
+        word += String.fromCharCode(parseInt(Math.random() * (122 - 97)) + 97); // a-z
+      }
+
+      for (var i = 0; i < Math.random() * 100; i++) {
+        result.push(word);
+        count--;
+      }
+    }
+    return result;
+  },
+  'lightlyOverlappingObjectIds': function (count) {
+    var result = [];
+
+    while (count > 0) {
+      result.push();
+      var word = '';
+      for (var j = 0; j < 24; j++) {
+        word += '012456789abcdef'[Math.floor(Math.random() * 16)];
+      }
+
+      for (var i = 0; i < Math.random() * 2; i++) {
+        result.push(word);
+        count--;
+      }
+    }
+    return result;
+  },
   'naiveCardinality': function (arr) {
     var t = {}, r = 0;
     for (var i = 0, l = arr.length; i < l; i++) {
@@ -24,19 +40,19 @@ module.exports = {
         r++;
       }
     }
-    return r;
+    return {'size': function () { return r; }};
   },
-  'compare': function (f1, f2, sizes) {
+  'compare': function (inputs, f1, f2, sizes) {
     var results = [];
     for (var index in sizes) {
-      var words = generateWords(sizes[index]);
+      var data = inputs(sizes[index]);
       
       var f1Start = Date.now();
-      var f1Count = f1(words);
+      var f1Count = f1(data).size();
       var f1End = Date.now();
       
       var f2Start = Date.now();
-      var f2Count = f2(words);
+      var f2Count = f2(data).size();
       var f2End = Date.now();
       results.push({
         'counts': [f1Count, f2Count],
